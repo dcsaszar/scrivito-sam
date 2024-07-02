94fa1ef7195d234c88e30a35aedce7c8095acc6c
Timothé VANNIER <timothe.vannier@gmail.com>
2024-07-02 10:37:00 +0200

    wip test stream

--- src/Components/ScrivitoExtensions/ChatbotTab/useChatCompletion.js
+++ src/Components/ScrivitoExtensions/ChatbotTab/useChatCompletion.js
@@ -168,25 +168,26 @@ async function mistralStreaming({
 
   const client = new MistralClient(apiKey);
 
-  const response = await client.chat({
+  const response = await client.chatStream({
     model,
     messages,
+    stream: true,
   });
 
-  // console.log('Chat Stream:');
-  // let fullMessage = '';
-  // for await (const chunk of response) {
-  //   const message = chunk.choices[0]?.delta?.content;
-  //   if (message) {
-  //     fullMessage += message;
-  //     setCompletionMessage(fullMessage);
-  //   }
-  // }
-  //
-  // console.log(response);
-  // console.log(fullMessage);
+  console.log('Chat Stream:');
+  let fullMessage = '';
+  for await (const chunk of response) {
+    const message = chunk.choices[0]?.delta?.content;
+    if (message) {
+      fullMessage += message;
+      setCompletionMessage(fullMessage);
+    }
+  }
 
   console.log(response);
+  console.log(fullMessage);
+
+  //console.log(response);
 
   setCompletionMessage(null);
   setMessages(messages.concat(response.choices[0].message));

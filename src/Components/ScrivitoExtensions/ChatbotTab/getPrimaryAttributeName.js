c5af181e10b82363b0760b2109feb65f57b9e4cd
Timothé VANNIER <timothe.vannier@gmail.com>
2024-07-15 17:37:23 +0200

    change entries

--- src/Components/ScrivitoExtensions/ChatbotTab/getPrimaryAttributeName.js
+++ src/Components/ScrivitoExtensions/ChatbotTab/getPrimaryAttributeName.js
@@ -1,6 +1,11 @@
 export function getPrimaryAttributeName(content) {
   console.log(content);
-  const entries = Object.entries(content.attributeDefinitions());
+  let entries = {};
+  if (content.nestedContent){
+    entries = Object.entries(content.widget.attributeDefinitions());
+  }else{
+    entries = Object.entries(content.attributeDefinitions());
+  }
   console.log(entries);
   return ["html", "string", "link"]
     .flatMap((type) =>

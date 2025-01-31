6d6659922a32b63b79b0e1cde9dcd2b8bbc3da43
David Császár <dcsaszar@users.noreply.github.com>
2025-01-31 13:04:45 +0100

    Fix JS error

--- src/Components/ScrivitoExtensions/model.js
+++ src/Components/ScrivitoExtensions/model.js
@@ -12,10 +12,10 @@ export function setModel(model) {
   localStorage.setItem("scrivito_ai_model", model);
 }
 
+const DEFAULT_MODEL = "aws/anthropic.claude-3-5-sonnet-20240620-v1:0";
+
 export const availableModels = [
   DEFAULT_MODEL,
   "gpt-4o-mini",
   "chatgpt-4o-latest",
 ];
-
-const DEFAULT_MODEL = "aws/anthropic.claude-3-5-sonnet-20240620-v1:0";
